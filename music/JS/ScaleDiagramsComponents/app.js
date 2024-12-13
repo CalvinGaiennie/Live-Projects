53fb20eb6c278ba7e9d72d476b25c6eff17005cc
CalvinGaiennie <calvingaiennie@gmail.com>
2024-12-13 12:28:43 -0600

    forgot to push last night

--- music/JS/ScaleDiagramsComponents/app.js
+++
@@ -1,9 +0,0 @@
-import { setupFretboard } from "./fretboard.js";
-import { setupEventListeners } from "./events.js";
-
-export const app = {
-  init() {
-    setupFretboard();
-    setupEventListeners();
-  },
-};
--- music/JS/ScaleDiagramsComponents/events.js
+++
@@ -1,4 +0,0 @@
-export const setupEventListeners = () => {
-  // Example event listener setup
-  console.log("Event listeners set up.");
-};
